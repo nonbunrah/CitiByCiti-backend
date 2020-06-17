7d6e36d48ce4a6b91cd369ae7ff7785e749f6774
nonbunrah <rj.bamrah@gmail.com>
2020-06-17 14:16:34 -0700

    added full CRUD functionality for user tbl

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/api/users', (req, res) => {
 })
 
 // Get one user
-app.get('/api/user/:id', (req, res) => {
+app.get('/api/users/:id', (req, res) => {
   const userId = req.params.id;
   const getUser = 
   `SELECT oid, * FROM tblUser WHERE tblUser.oid = ?`;
@@ -47,6 +47,56 @@ app.get('/api/user/:id', (req, res) => {
       res.sendStatus(500);
     }
     res.status(200).json(results);
+  })
+});
+
+// add user
+app.post('/api/users', (req, res) => {
+  const reqBody = [req.body.firstname, req.body.lastname, req.body.email];
+  const createNewUser = `INSERT INTO tblUser VALUES (?, ?, ?)`;
+  console.log(req.body)
+  database.run(createNewUser, reqBody, (error, results) => {
+    if (error) {
+      console.log('Error adding new user: ' + req.body.firstname + ' ' + req.body.lastname)
+      res.sendStatus(500)
+    } else {
+      console.log('Added new user: ' + req.body.firstname + ' ' + req.body.lastname)
+      res.sendStatus(200)
+    }
+  })
+})
+
+//update user
+app.put('/api/users/:id',(req, res) => {
+  const userId = parseInt(req.params.id);
+  const queryHelper = Object.keys(req.body).map(element => `${element.toUpperCase() } = ?`);
+  const updateOneUser = `UPDATE tblUser SET ${queryHelper.join(', ')} WHERE tblUser.oid = ?`;
+  const queryValues = [...Object.values(req.body), userId];
+
+  database.run(updateOneUser, queryValues, function (error) {
+    if (error) {
+      console.log(new Error('Could not update user'), error);
+      res.sendStatus(500);
+    } else {
+      console.log('User: ' + req.body.firstname + ' ' + req.body.lastname + ' was successfully updated');
+      res.sendStatus(200);
+    }
+  })
+});
+
+// delete user
+app.delete('/api/users/:id', (req, res) => {
+  const userId = req.params.id
+  const getUser = `DELETE FROM tblUser WHERE tblUser.oid = ?`;
+
+  database.all(getUser, [userId], (error, results) => {
+    if (error) {
+      console.log(new Error('Could not delete user'), error);
+      res.sendStatus(500);
+    } else {
+      console.log("User was successfully deleted");
+      res.status(200).json({message: "Delete successful"});
+    }
   })
 });
 
